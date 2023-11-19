aa350ac62b7cac62a26a3d21340ab60389d556f6
Rayhan <44944844+ToxicBeastt@users.noreply.github.com>
2023-11-19 16:09:58 +0700

    fix: update layouiting

--- src/views/Home/ListView.js
+++ src/views/Home/ListView.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Button from '../../components/Button';
-import { Link } from 'react-router-dom';
-import { Card, CardContent, CardMedia, Typography } from '@mui/material';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Button from "../../components/Button";
+import { Link } from "react-router-dom";
+import { Card, CardContent, CardMedia, Grid, Typography } from "@mui/material";
 
 const ListView = () => {
   const [pokemonList, setPokemonList] = useState([]);
@@ -10,7 +10,11 @@ const ListView = () => {
 
   useEffect(() => {
     const fetchPokemonList = async () => {
-      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${(currentPage - 1) * 10}&limit=10`);
+      const response = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon?offset=${
+          (currentPage - 1) * 10
+        }&limit=10`
+      );
       setPokemonList(response.data.results);
     };
 
@@ -18,38 +22,51 @@ const ListView = () => {
   }, [currentPage]);
 
   return (
-    <div className="m-8">
-      <div className='flex flex-wrap justify-center gap-4'>
-        {pokemonList.map((pokemon) => (
-          <Card key={pokemon.name} style={{ maxWidth: 300, margin: '1em' }}>
-            <CardMedia
-              component="img"
-              height="140"
-              image={`https://img.pokemondb.net/artwork/${pokemon.name}.jpg`}
-              alt={pokemon.name}
-            />
-            <CardContent>
-              <Typography variant="h6" component="div">
-                {pokemon.name}
-              </Typography>
-              <Link to={`/pokemon/${pokemon.name}`}>View Details</Link>
-            </CardContent>
-          </Card>
-        ))}
+    <div className="mx-4 my-2">
+      <div className="flex flex-wrap justify-center gap-4">
+        <Grid container spacing={2}>
+          {pokemonList.map((pokemon, index) => (
+            <Grid item key={index}>
+              <Card
+                key={pokemon.name}
+                sx={{
+                  maxWidth: 200,
+                  height: 350,
+                  margin: "0 auto",
+                  padding: "0.1em",
+                }}
+              >
+                <CardMedia
+                  component="img"
+                  height="150"
+                  width="100"
+                  image={`https://img.pokemondb.net/artwork/${pokemon.name}.jpg`}
+                  alt={pokemon.name}
+                  sx={{ height: 250, width: 300, objectFit: "contain" }}
+                />
+                <CardMedia></CardMedia>
+                <CardContent>
+                  <Typography variant="h6" component="div">
+                    {pokemon.name}
+                  </Typography>
+                  <Link to={`/pokemon/${pokemon.name}`}>View Details</Link>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </div>
-      <div className='flex justify-end mt-4'>
+      <div className="flex justify-end mt-4">
         <div className="flex items-center gap-2">
-          <Button 
+          <Button
             onClick={() => setCurrentPage((prevPage) => prevPage - 1)}
             disabled={currentPage === 1}
-            >
+          >
             Previous
           </Button>
           <span> Page {currentPage} </span>
-          <Button 
-            onClick={() => setCurrentPage((prevPage) => prevPage + 1)}
-            >
-              Next
+          <Button onClick={() => setCurrentPage((prevPage) => prevPage + 1)}>
+            Next
           </Button>
         </div>
       </div>
